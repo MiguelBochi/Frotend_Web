70d8d6d2d4cfaf972837a390315ae1d6b14791fc
MiguelBochi <miguelbochibarro@gmail.com>
2023-11-14 08:55:55 -0300

    login

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 /*const MY_IP = import.meta.env.VITE_MY_IP*/
 
 const api = axios.create({
-  baseURL: `http://localhost:19003/api/`,
-  // baseURL: 'http://192.168.0.17:19003/api/',
+  baseURL: `https://grandeorvalho-dev-scbe.3.us-1.fl0.io/api/`,
 })
 
 export default api
